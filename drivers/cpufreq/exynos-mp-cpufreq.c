7a430c18624077bc60709c8770936baaf69c5d9c
engine95 <mustangsroc@gmail.com>
2017-08-23 16:35:29 -0600

    Raised voltages for higher CPU clocks

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -1385,8 +1385,8 @@ static size_t get_freq_table_size(struct cpufreq_frequency_table *freq_table)
 }
 
 #ifdef CONFIG_SOC_EXYNOS5433
-#define KFC_MAX_VOLT 1200000
-#define EGL_MAX_VOLT 1300000
+#define KFC_MAX_VOLT 1280000
+#define EGL_MAX_VOLT 1360000
 #else
 #warning "Please define core maximum voltages for current SoC."
 #define KFC_MAX_VOLT 1300000
