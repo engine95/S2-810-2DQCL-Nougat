13d32bfaa1f0880c4e364153c6d851814a43a4e3
engine95 <mustangsroc@gmail.com>
2017-09-19 06:46:17 -0600

    Revert "mmc: fix host release issue after discard operation"
    This reverts commit eaecbb2a65098e822f16c82d0664fbe83bcd2b15.

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2353,7 +2353,6 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	struct mmc_host *host = card->host;
 	unsigned int cmd_flags = req ? req->cmd_flags : 0;
 	unsigned long flags;
-	unsigned int cmd_flags = req ? req->cmd_flags : 0;
 
 	if (card->ext_csd.cmdq_mode_en) {
 		mmc_claim_host(card->host);
@@ -2391,6 +2390,8 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 			ret = mmc_blk_issue_secdiscard_rq(mq, req);
 		else
 			ret = mmc_blk_issue_discard_rq(mq, req);
+		if (card->ext_csd.cmdq_mode_en)
+			mmc_release_host(card->host);
 	} else if (cmd_flags & REQ_FLUSH) {
 		/* complete ongoing async transfer before issuing flush */
 		if (card->host->areq || card->ext_csd.cmdq_mode_en)
@@ -2416,15 +2417,17 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 	}
 
 out:
-	if ((!req && !(mq->flags & MMC_QUEUE_NEW_REQUEST)) ||
-	     (cmd_flags & MMC_REQ_SPECIAL_MASK))
-		/*
-		 * Release host when there are no more requests
-		 * and after special request(discard, flush) is done.
-		 * In case sepecial request, there is no reentry to
-		 * the 'mmc_blk_issue_rq' with 'mqrq_prev->req'.
-		 */
-		mmc_release_host(card->host);
+	if (!card->ext_csd.cmdq_mode_en) {
+		if ((!req && !(mq->flags & MMC_QUEUE_NEW_REQUEST)) ||
+		     (cmd_flags & MMC_REQ_SPECIAL_MASK))
+			/*
+			 * Release host when there are no more requests
+			 * and after special request(discard, flush) is done.
+			 * In case sepecial request, there is no reentry to
+			 * the 'mmc_blk_issue_rq' with 'mqrq_prev->req'.
+			 */
+			mmc_release_host(card->host);
+	}
 	return ret;
 }
 
